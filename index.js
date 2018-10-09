3517dc5dc9fba09c691bd2b51b98d53a8386bb84
George Cheng <Gerhut@GMail.com>
2018-10-09 20:18:24 +0800

    use params to transfer access token

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ const querystring = require('querystring');
 
 const weibo = axios.create({
   baseURL: 'https://api.weibo.com/2',
-  params: { access_token: process.env.WEIBO_ACCESS_TOKEN },
   maxRedirects: 0
 })
 
 const router = new Router()
-router.get('/', async ({ response }) => {
+router.get('/:accessToken', async ({ params, response }) => {
   try {
-    const { data } = await weibo.get('/account/rate_limit_status.json')
+    const { data } = await weibo.get('/account/rate_limit_status.json', {
+      params: { access_token: params.accessToken }
+    })
     response.body = data
   } catch (error) {
     debug(error)
@@ -25,17 +26,19 @@ router.get('/', async ({ response }) => {
     }
   }
 })
-router.post('/', bodyparser({
+router.post('/:accessToken', bodyparser({
   enableTypes: [ 'text' ]
-}), async ({ request, response }) => {
+}), async ({ params, request, response }) => {
   try {
     const { data } = await weibo.post('statuses/share.json', querystring.stringify({
       status: request.body,
       rip: request.ip
-    }))
+    }), {
+      params: { access_token: params.accessToken }
+    })
     response.body = data
   } catch (error) {
-    debug(error.config)
+    debug(error)
     response.status = 500
     if (error.response) {
       response.body = error.response.data
